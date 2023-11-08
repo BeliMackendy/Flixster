5c10a13fef2f2c41f625f6c177a57283ca0d6c55
BeliMackendy <belizaire.mackendy@gmail.com>
2023-11-08 14:53:56 -0500

    Networking and Parsing JSON

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,42 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private String overview;
+    private String posterPath;
+
+    public Movie(JSONObject movieObject) throws JSONException {
+        title = movieObject.getString("title");
+        overview = movieObject.getString("overview");
+        posterPath = movieObject.getString("poster_path");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieArray) throws JSONException {
+        List<Movie> movieList = new ArrayList<>();
+        for(int i = 0; i< movieArray.length(); i++){
+            movieList.add(new Movie(movieArray.getJSONObject(i)));
+        }
+
+        return movieList;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+}
+
